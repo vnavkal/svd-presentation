99379108451df1efb7ac692c1c55857746836e82
Viraj <vnavkal0@gmail.com>
2018-01-08 12:41:45 -0800

    more changes

--- aifi_graph_coding_exercise.py
+++ aifi_graph_coding_exercise.py
@@ -14,51 +14,60 @@ def loop_exists(edges):
     """
     if edges:
         start_vertex = next(iter(edges))[0]
-        edges_traversed, loop_found = find_loops_from_start_vertex(start_vertex, edges)
+        edges_traversed, loop_found = (
+            find_loops_from_start_vertex(start_vertex, edges))
         if loop_found:
             return True
         else:
-            new_edges = edges - edges_traversed
-            return loop_exists(new_edges)
+            remaining_edges = edges - edges_traversed
+            return loop_exists(remaining_edges)
     else:
         return False
 
 
 def find_loops_from_start_vertex(start_vertex, edges, edges_found=set()):
-    edges_starting_at_vertex = [edge for edge in edges if edge[0] == start_vertex]
+    """Traverse subgraph starting at `start_vertex`, checking for cycles"""
+    edges_starting_at_vertex = [edge for edge in edges
+                                if edge[0] == start_vertex]
     for edge in edges_starting_at_vertex:
         if edge in edges_found:
-            return edges_found, True  # found a loop
+            return edges_found, True  # found a cycle
         else:
             edges_found = edges_found | {edge}
-            edges_found, loop_found = find_loops_from_start_vertex(edge[1], edges, edges_found)
-            if loop_found:
+            edges_found, loop_found = (
+                find_loops_from_start_vertex(edge[1], edges, edges_found))
+            if loop_found:  # subgraph traversal found a cycle
                 return edges_found, True
     return edges_found, False
 
 
 class TestLoopExists(unittest.TestCase):
-    def test_with_no_edges(self):
+    def test_no_edges(self):
         edges = {}
         self.assertFalse(loop_exists(edges))
 
-    def test_with_one_edge(self):
+    def test_one_edge(self):
         edges = {('V', 'W')}
         self.assertFalse(loop_exists(edges))
 
-    def test_with_0_cycle(self):
+    def test_cycle_of_length_1(self):
         edges = {('V', 'V')}
         self.assertTrue(loop_exists(edges))
 
-    def test_with_1_cycle(self):
+    def test_cycle_of_length_2(self):
         edges = {('V', 'W'), ('W', 'V')}
         self.assertTrue(loop_exists(edges))
 
-    def test_with_complicated_loopless_graph(self):
-        edges = {('U', 'V'), ('V', 'W'), ('X', 'W'), ('Y', 'W'), ('Y', 'X')}
+    def test_cycle_of_length_4(self):
+        edges = {('U', 'V'), ('V', 'W'), ('W', 'X'), ('X', 'U')}
+        self.assertTrue(loop_exists(edges))
+
+    def test_complicated_loopless_graph(self):
+        edges = {('U', 'V'), ('V', 'W'), ('X', 'W'),
+                 ('Y', 'W'), ('Y', 'X'), ('W', 'Z')}
         self.assertFalse(loop_exists(edges))
 
-    def test_with_complicated_graph_with_loop(self):
+    def test_complicated_graph_with_loop(self):
         edges = {('U', 'V'), ('V', 'W'), ('X', 'W'),
                  ('Y', 'W'), ('Y', 'X'), ('W', 'U')}
         self.assertTrue(loop_exists(edges))
